a0cd3ae80dfc38cbc521ff930bce29fbb0b00a42
Graham Lounder <graham@dashhudson.com>
2021-11-03 15:23:53 -0300

    Added list display

---
+++ src/components/List.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="list-container">
+        <template v-for="(item, index) in items">
+            <div :key="`item-${index}`" class="item-details">
+                <div class="main-content-panel">
+                    <slot :item="item" />
+                </div>
+
+                <div class="action-panel">
+                    <slot name="action-content" :item="item" />
+                </div>
+            </div>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    /**
+     * items to display in the list
+     */
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.list-container {
+  overflow: auto;
+}
+
+.item-details {
+  display: flex;
+  flex-grow: 1;
+  border-radius: 0.25rem;
+  padding: 1.375rem 0 1.25rem 0;
+  align-items: center;
+}
+
+.main-content-panel {
+  margin-left: 1.5rem;
+  margin-right: 1.5rem;
+  display: flex;
+  flex-direction: row;
+}
+
+.action-panel {
+  margin-left: auto;
+  margin-right: 1.5rem;
+  display: flex;
+  flex-direction: row;
+}
+
+.item-details:nth-child(odd) {
+  background-color: #f8f8f8;
+}
+</style>
--- src/components/Listings.vue
+++ src/components/Listings.vue
@@ -1,16 +1,24 @@
 <template>
     <div>
         <h3>Listing</h3>
-        <template v-for="listing in listings">
-            <div v-if="listing" :key="listing.id">{{ listing.id }} {{ listing.property_type }}</div>
-        </template>
+        <List :items="listings" #default="{ item: listing }">
+            <div>
+                <div>Property: {{ listing.property_type }}</div>
+                <div>Place: {{ listing.place_type }}</div>
+                <div>Occupancy: {{ listing.occupancy }}</div>
+            </div>
+        </List>
     </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import List from './List.vue';
 
 export default {
+    components: {
+        List,
+    },
     computed: {
         ...mapState(['listings']),
     },
