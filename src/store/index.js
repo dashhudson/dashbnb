e795a36e948871a54a7723eee46e40caee728301
Graham Lounder <graham@dashhudson.com>
2021-11-15 13:36:46 -0400

    Added listings unit test

--- src/store/index.js
+++ src/store/index.js
@@ -1,66 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import apis from '../apis';
+import StorgConfig from './store.config';
 
 Vue.use(Vuex)
 
-const AUTH_TOKEN_NAME = 'authToken';
-const DEFAULT_PAGE = 'listings';
-
-export default new Vuex.Store({
-  state: {
-    authToken: null,
-    currentPage: null,
-    listings: [],
-  },
-  getters: {
-    isLoggedIn(state) {
-      return !!state.authToken;
-    },
-    currentPage(state) {
-      return state.currentPage;
-    },
-  },
-  mutations: {
-    setAuthToken(state, newToken) {
-      state.authToken = newToken;
-      apis.setToken(newToken);
-      if (newToken) {
-        localStorage.setItem(AUTH_TOKEN_NAME, newToken);
-      } else {
-        localStorage.removeItem(AUTH_TOKEN_NAME);
-      }
-    },
-    setListings(state, newListings) {
-      state.listings = newListings;
-    },
-    setCurrentPage(state, page) {
-      state.currentPage = page;
-    }
-  },
-  actions: {
-    async init({ commit }) {
-      const storedToken = localStorage.getItem(AUTH_TOKEN_NAME);
-      if (storedToken) {
-        commit('setAuthToken', storedToken);
-      }
-      commit('setCurrentPage', DEFAULT_PAGE);
-    },
-    async login({ commit }, { email, password }) {
-        const response = await apis.login({ email, password });
-        commit('setAuthToken', response.data.authToken);
-    },
-    async logout({ commit }) {
-      commit('setAuthToken', null);
-    },
-    async getListings({ commit }) {
-      const response = await apis.getListings();
-      commit('setListings', response.data.items);
-    },
-    async gotoPage({ commit }, page) {
-      commit('setCurrentPage', page);
-    }
-  },
-  modules: {
-  }
-})
+export default new Vuex.Store(StorgConfig)
